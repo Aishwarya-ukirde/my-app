a27b05d348eaf80c5daac8e8d8b7b73547605f0a
Aishwarya-ukirde <114334826+Aishwarya-ukirde@users.noreply.github.com>
2024-08-02 11:48:13 +0530

    Update Slice.js

--- src/app/redux/Slice.js
+++ src/app/redux/Slice.js
@@ -1,7 +1,7 @@
 import { createSlice, nanoid , current } from '@reduxjs/toolkit';
 
 const initialState ={
-    users: JSON.parse(localStorage.getItem("users")) ? JSON.parse(localStorage.getItem("users")) : []
+    users: JSON.parse(localStorage.getItem("users")) || []
 }
 
 const Slice = createSlice({
